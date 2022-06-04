de05147deeacafbe392c7d31cc0e12cdea562253
MylesBolton <38015231+MylesBolton@users.noreply.github.com>
2022-06-04 18:17:10 +0100

    Create Screen.sh

---
+++ Screen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+JAR=spigot-1.18.2.jar
+MAXRAM=12G
+MINRAM=4G
+TIME=20
+
+while [ true ]; do
+    java -Xmx$MAXRAM -Xms$MINRAM -jar $JAR nogui
+    if [[ ! -d "exit_codes" ]]; then
+        mkdir "exit_codes";
+    fi
+    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+        touch "exit_codes/server_exit_codes.log";
+    fi
+    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+    echo "----- Press enter to prevent the server from restarting in $TIME seconds -----";
+    read -t $TIME input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "------------------- SERVER RESTARTS -------------------";
+    fi
+done
